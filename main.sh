15abbabb426c2e68419d41f365cfb63ce4121bfe
ferrreo <harderthanfire@gmail.com>
2023-07-28 21:03:37 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ cd ./gamescope
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p gamescope_3.12.0
+LOGNAME=root dh_make --createorig -y -l -p gamescope_3.12.0-release
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
